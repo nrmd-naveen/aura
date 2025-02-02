7fa3ef24ff87b903004330dbea293218d03cd4ba
nrmd-naveen <nrmdnaveen@gmail.com>
2025-02-02 23:58:20 +0530

    Nav Bar Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,58 @@
-import AURA from "./assets/svg/AURA_TEXT"
-import Button from "./assets/ui/Button"
-import GraphSVG from "./assets/svg/GraphSVG"
-import HeroBG from "./assets/svg/HeroBG"
-import VideoCarousel from "./assets/ui/VideoCarousel"
+import NavBar from "./assets/ui/NavBar"
 
 
 function App() {
 
   return (
     <>
-      <div className="bg-primary overflow-hidden">
-        <div className="w-screen h-screen relative flex justify-center">
-          <div className="h-screen w-screen overflow-hidden z-0 ">
-            <HeroBG />
-          </div>
-          <div className="absolute top-[5%] z-10">
-            <h1 className="h-auto font-league  text-[12rem] sm:text-[20rem]  lg:text-[35rem] leading-72 tracking-tighter font-bold text-white opacity-20">aura</h1>
-            {/* <AURA opacity={0.2}  /> */}
-            <div className="flex justify-center md:mt-40 gap-5 -z-40">
-              <Button
-                variant="primary"
-                text="Get Started"
-                onClick={() => alert("Get Started")}
-                />
-              <Button
-                variant="secondary"
-                text="Our Works"
-                onClick={() => alert("Our Works")}
-                />
-              </div>
-          </div>
-          
-          <div className=" absolute z-20 bottom-[10%]">
-            <GraphSVG />
-          </div>
-        </div>
-
-        <VideoCarousel />
-        
-
-      </div>
+      <div className="w-screen overflow-hidden h-screen bg-primary">
+        <NavBar />
+    </div>
     </>
   )
+  
 }
 
 export default App
+
+
+
+
+// return (
+//     <>
+//       <div className="w-screen h-auto  bg-primary overflow-hidden">
+//         <NavBar />
+//         {/* <NavBar2 /> */}
+//         <div className=" relative flex justify-center">
+//           <div className="h-screen w-screen overflow-hidden z-0 ">
+//             <HeroBG />
+//           </div>
+//           <div className="absolute top-[5%] z-10">
+//             <h1 className="h-auto font-league  text-[12rem] sm:text-[20rem]  lg:text-[35rem] leading-72 tracking-tighter font-bold text-white opacity-20">aura</h1>
+//             {/* <AURA opacity={0.2}  /> */}
+//             <div className="flex justify-center md:mt-40 gap-5 -z-40">
+//               <Button
+//                 variant="primary"
+//                 text="Get Started"
+//                 onClick={() => alert("Get Started")}
+//                 />
+//               <Button
+//                 variant="secondary"
+//                 text="Our Works"
+//                 onClick={() => alert("Our Works")}
+//                 />
+//               </div>
+//           </div>
+          
+//           <div className=" absolute z-20 bottom-[10%]">
+//             <GraphSVG />
+//           </div>
+//         </div>
+
+//         <div className="h-32"></div>
+//         <VideoCarousel />
+        
+//         <div className="h-72"></div>
+//       </div>
+//     </>
+//   )
---
+++ src/assets/ui/NavBar.tsx
@@ -0,0 +1,61 @@
+import LogoSVG from "../svg/LogoSVG";
+import Button from "./Button";
+
+const NavBar = () => {
+    return (
+        <>
+            {/* <div className="fixed  w-full h-20 bg-transparent py-2 px-20 ">
+                <div className="z-50 w-full h-full "></div>
+            </div> */}
+            <div className="p-2">
+                <Header />
+            </div>
+        </>
+  )
+};
+
+
+const menus = [
+    {
+        name: "Projects",
+        href: "#projects",
+    },
+    {
+        name: "ContactUs",
+        href: "#contact",
+    }
+]
+
+
+const Header = () => {
+
+    return <>
+        <div className=" p-2 px-6 md:p-6 z-40  flex justify-center items-center h-20">
+            <header className="z-40 fixed md:w-auto flex jutisfy-between items-center h-[55px] pl-4 px-2  bg-white/10 backdrop-blur-lg border-mywhite-border shadow-md rounded-xl">
+                <div className="flex justify-between w-full h-full gap-4">
+                <div className=" h-full flex items-center ">
+                <LogoSVG />
+                </div>
+                <div className="flex gap-0 ">
+                    {
+                            menus.map((menu, index) => (
+                            <a
+                                    className="h-full flex justify-center items-center"
+                                    href={menu.href}
+                                    key={index}
+                            >
+                                    
+                            <span
+                                className="text-white font-barlow text-sm md:text-base px-4 py-3 rounded-full"   
+                        >{menu.name}
+                        </span></a>
+                        ))
+                    }
+                </div>
+                </div>
+            </header>
+        </div>
+    </>
+}
+
+export default NavBar;
